b546235a4dabc0a7290571b14a6968e57736272b
karsim31 <karsim31121970@gmail.com>
2024-06-30 15:00:32 +0700

    Add files via upload

---
+++ hicas.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+folder_path="/home/hicasaci/public_html"
+backup_path="/tmp"
+
+while true
+do
+    if [ ! -d "$folder_path" ]; then
+        cp -r "$backup_path" "$folder_path"
+    fi
+
+    chmod -R 0755 "$folder_path"
+    sleep 1
+done

